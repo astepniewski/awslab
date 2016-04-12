4969e73cfbe6f9aca9eefccde6a14b23b2a3e475
astepniewski <a.stepniewski@hotmail.com>
2016-04-12 11:14:01 +0200

    Add lab1_2 to app.j and add simple action in this file

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -1,11 +1,10 @@
 //stub for lab 1_2
 var AWS = require('aws-sdk');
-
 AWS.config.loadFromPath('./config.json');
 
 var task =  function(request, callback){
 	
-	
+	callback(null, "Arek");
 }
 
 exports.lab = task
