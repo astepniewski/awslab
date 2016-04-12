e8c16d2e180e3aa05ecb47dff5fe214379490cff
astepniewski <a.stepniewski@hotmail.com>
2016-04-12 11:30:08 +0200

    display info about ec2 instance

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -1,10 +1,18 @@
 //stub for lab 1_2
 var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
+var ec2 = new AWS.EC2();
 
 var task =  function(request, callback){
-	
-	callback(null, "Arek");
+	var params = {
+  InstanceIds: [
+    'i-a3b3ed7b',
+  ]
+};
+ec2.describeInstances(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     callback(null, data);           // successful response
+});
 }
 
 exports.lab = task
